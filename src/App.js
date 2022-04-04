808e09df3ad566350a31b523048fb56ca23d4cf4
sheng <sleep_qzs@icloud.com>
2022-04-04 17:01:27 +0800

    react 18

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+/*
+ * @Author: your name
+ * @Date: 2022-04-04 15:58:23
+ * @LastEditTime: 2022-04-04 16:04:48
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /my-app/src/App.js
+ */
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function Example() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
 }
 
-export default App;
+export default Example;
