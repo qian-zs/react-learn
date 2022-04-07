f3651a4df93550eb8989a0e2b0d5e3a2229532ba
sheng <sleep_qzs@icloud.com>
2022-04-07 11:23:04 +0800

    commit

---
+++ src/App copy 53.js
@@ -0,0 +1,19 @@
+import { useState, useEffect, useMemo } from 'react';
+
+const App = () => {
+    const [count, setCount] = useState(0);
+    const [obj, setObj] = useState({ id: 1 });
+
+    return (
+        <>
+            {/* 普通更新 */}
+            <div>count: {count}</div>
+            <button onClick={() => setCount(count + 1)}>add</button>
+            {/* 函数式更新 */}
+            <div>obj: {JSON.stringify(obj)}</div>
+            <button onClick={() => setObj((prevObj) => ({ ...prevObj, ...{ id: 2, name: "张三" } }))}>merge</button>
+        </>
+    )
+};
+
+export default App;
