1910019574d103589798cc0627db6236ca0a65ad
sheng <sleep_qzs@icloud.com>
2022-06-21 13:03:30 +0800

    commit

--- src/App copy 64.js
+++ src/App copy 64.js
@@ -6,21 +6,10 @@
  * @Description: 要实现具名插槽可以从父组件中传递一个对像给子组件，这个对象中的内容可以是jsx，也可以是一段文本内容，还可以是一个事件方法;
  * @FilePath: /my-app/src/App.js
  */
-import Navbar from './useContext/Navbar';
-import Messages from './useContext/Messages';
-import React from 'react';
+import Layout from "./namedSlot/Layout";
 
 export default function App() {
-    const AppContext = React.createContext({});
-
     return (
-        <AppContext.Provider value={{
-            username: 'superawesome'
-        }}>
-            <div className="App">
-                <Navbar />
-                <Messages />
-            </div>
-        </AppContext.Provider>
-    );
+        <Layout />
+    )
 }
